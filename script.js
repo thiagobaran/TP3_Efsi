b8c48267e8f6602abc0857e81cf9dfed7c71c2fa
SANTINO ARANA <47031761@ort>
2023-04-27 10:23:23 -0300

    terminado

--- script.js
+++ script.js
@@ -24,23 +24,8 @@ botonBuscar.onclick = () =>{
         console.log(res.data.products);
         res.data.products.forEach(element => {
             
-            /*const comienzoCard = document.getElementById("comienzoCard");
-            const cardProd = document.getElementById("cardProd");
-            const unaImg = document.createElement("img");
-            const titleProd = document.createElement("h5");
-            const descProd = document.createElement("p");
-            const cBody = document.getElementById("cBody");
-            const body = document.createElement("div");
-
-
-            unaImg.innerHTML = `<img src ${element.images} class="card-img-top" alt="...">`;
-
-            titleProd.innerHTML = `<h5> ${element.title} </h5>`;
-            descProd.innerHTML = `<p> ${element.description} </p>`;
-            body.innerHTML=`<h5> ${element.title} </h5> <p> ${element.description} </p>`;*/
             const unDiv = document.createElement("div");
             unDiv.classList.add("col-md-3");
-
 
             unDiv.innerHTML = `
             <div class="card">
@@ -48,7 +33,7 @@ botonBuscar.onclick = () =>{
                 <div class="card-body">
                 <h5 class="card-title">${element.title}</h5>
                 <p class="card-text">${element.description}</p>
-                <a href="#" class="btn btn-primary" onclick="verDetalle('${element.title}${element.price}${element.discountPercentage}${element.rating}${element.stock}${element.brand}${element.category}')">Ver detalle</a>
+                <a href="#" class="btn btn-dark" onclick="verDetalle('${element.title}','${element.price}','${element.discountPercentage}','${element.rating}','${element.stock}','${element.brand}','${element.category}')">Ver detalle</a>
                 </div>
             </div>
             `;
@@ -63,21 +48,22 @@ botonBuscar.onclick = () =>{
 
 
 const verDetalle = (nombre,precio,porc,rating,stock,marca,categoria) => {
-    <div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+    const divTitulo = document.getElementById("headerModal");
+    const bodyModal = document.getElementById("bodyModal");
+    divTitulo.innerHTML="";
+    bodyModal.innerHTML="";
+    const titulo = document.createElement("h5");
+    titulo.classList.add("modal-title");
+    const texto = document.createElement("p");
+
+    titulo.innerHTML = nombre;
+    texto.innerHTML = "Precio: "+ precio + '<br>' + "Porcentaje de descuento: " + porc +'<br>' + "Rating: " + rating +'<br>' + "Stock: " + stock +'<br>' + "Marca: " + marca +'<br>' + "Categoria: " + categoria;
+
+    
+    divTitulo.appendChild(titulo);
+    bodyModal.appendChild(texto);
+    const myModal = new bootstrap.Modal('#sa', {
+        keyboard: false
+    })
+    myModal.show(true);
 }
